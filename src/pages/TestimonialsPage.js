c63e7e88e4c29f12dc1dfee8bb6ac6e5985524d3
Andy Phung <aphung@nationalboard.org>
2025-04-06 11:41:12 -0700

    updated testimonials quotations

--- src/pages/TestimonialsPage.js
+++ src/pages/TestimonialsPage.js
@@ -99,7 +99,7 @@ export const TestimonialsPage = ({ title, description }) => (
         <ServiceCard
           title="Eva Lopez. (Long Beach, CA)"
           description={
-            "I'm Eva Lopez I recommended Jessica she is very especial to work with insurance thank you for your help"
+            '"I\'m Eva Lopez I recommended Jessica she is very especial to work with insurance thank you for your help"'
           }
           image="/eva.webp"
           imageSide="left"
@@ -107,14 +107,14 @@ export const TestimonialsPage = ({ title, description }) => (
         <ServiceCard
           title="Devin P. (Apache Junction, AZ)"
           description={
-            '"I was so overwhelmed with everything related to Medicare when I turned 65 and ready to retire. I was inundated with mail from so many different insurance companies, phone calls and messages every day of the week and people coming to my home knocking on my door. A good friend of mine suggested I contact Jessica Tcheng for help. Jessica helped me every step of the way.  She spent a lot of time with me on the phone explaining everything in detail and the options available.  She contacted my doctors’ offices and found out what plans they accepted.  She sent me info on the plans she thought were the best for me to review and help with my choices. Jessica made the whole process so easy for me to understand and complete.   I just can’t say enough good things about her!! If you have any issues, problems, or questions about Medicare, I suggest you contact Jessica Q Tcheng @ www.itoinsuranceagency.com, Phone: (949) 394-7738, Fax: (949) 301-9611 She will help you at no cost!!"'
+            '"I was so overwhelmed with everything related to Medicare when I turned 65 and ready to retire. I was inundated with mail from so many different insurance companies, phone calls and messages every day of the week and people coming to my home knocking on my door. A good friend of mine suggested I contact Jessica Tcheng for help. Jessica helped me every step of the way. She spent a lot of time with me on the phone explaining everything in detail and the options available. She contacted my doctors’ offices and found out what plans they accepted. She sent me info on the plans she thought were the best for me to review and help with my choices. Jessica made the whole process so easy for me to understand and complete. I just can’t say enough good things about her!! If you have any issues, problems, or questions about Medicare, I suggest you contact Jessica Q Tcheng @ www.itoinsuranceagency.com, Phone: (949) 394-7738, Fax: (949) 301-9611 She will help you at no cost!!"'
           }
           imageSide="left"
         />
         <ServiceCard
           title="Esther & Ramon A. (Santa Clarita, CA)"
           description={
-            "She's very helpful, attentive and she treats me like she's helping a family member."
+            '"She\'s very helpful, attentive and she treats me like she\'s helping a family member."'
           }
           image="/esther.webp"
           imageSide="left"
@@ -168,7 +168,7 @@ export const TestimonialsPage = ({ title, description }) => (
         <ServiceCard
           title="William B. (Long Beach, CA)"
           description={
-            "ITO Insurance Agency is awesome!  Also Jessica Tcheng is awesome!  If you really want a good insurance plan better get with Jessica. Someone who really loves what she dose, her job. And she really love helping people, especially us seniors. I tell you the truth, there's been a couple of times I've gotten so frustrated trying to distinguish which insurance plan is a good insurance plan from going through quite a few? So I call Jessica for help, and I get help and no more frustration! She finds me a really good insurance plan. William Beck US Army Disabled Veteran"
+            '"ITO Insurance Agency is awesome! Also Jessica Tcheng is awesome! If you really want a good insurance plan better get with Jessica. Someone who really loves what she dose, her job. And she really love helping people, especially us seniors. I tell you the truth, there\'s been a couple of times I\'ve gotten so frustrated trying to distinguish which insurance plan is a good insurance plan from going through quite a few? So I call Jessica for help, and I get help and no more frustration! She finds me a really good insurance plan. William Beck US Army Disabled Veteran."'
           }
           image="/william.webp"
           imageSide="left"
